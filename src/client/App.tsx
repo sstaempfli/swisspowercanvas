d44789adf3451939c8eee362b8a294635fb8a730
Shana Stämpfli <Shana@MacBook-Pro-5.lan>
2023-11-30 17:34:09 +0100

    swiss map rendering complete

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Layout from "./Layout";
+import SwissMap from "./SwissMap";
+import { useSwissAtlas } from "./state/hooks";
 
 function App() {
+  const { features, cantonBorders, municipalityBorders } = useSwissAtlas();
   return (
-      <Layout>
-      TODO: implement me!
-      </Layout>
+    <Layout>
+      <SwissMap features= {features} cantons={ cantonBorders} municipalities= {municipalityBorders}  />
+    </Layout>
   );
 }
 
 export default App;
+
