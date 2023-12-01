3e9c4c425c1f5b6843202e343262daef27f0459c
Aurel <asostizzo@ethz.ch>
2023-12-01 15:04:08 +0100

    get data from server

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,11 +3,28 @@ import Layout from "./Layout";
 import SwissMap from "./SwissMap";
 import { useSwissAtlas } from "./state/hooks";
 
+
+
+const requestData = async () => {
+  // Use fetch to send the request to the server
+  try {
+    const response = await fetch("/hello");
+
+    // Process your server's response here
+    const data = await response.text();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  return;
+};
+
 function App() {
   const { features, cantonBorders, municipalityBorders } = useSwissAtlas();
   return (
     <Layout>
       <SwissMap features= {features} cantons={ cantonBorders} municipalities= {municipalityBorders}  />
+      <button onClick={requestData}>Very good on press button event</button>
     </Layout>
   );
 }
