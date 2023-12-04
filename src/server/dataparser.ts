bac65a33d9f04f6b3dfa1a53e09f7cd541eb2c98
Noah <noah.fecker@gmail.com>
2023-12-04 14:28:41 +0100

    add id to backend cantons

--- src/server/dataparser.ts
+++ src/server/dataparser.ts
@@ -6,30 +6,46 @@ interface PowerData {
     TotalPower: string;
 }
 
-let cantonPower: { [key: string]: number } = {};
+interface CantonID {
+    id: string;
+    name: string;
+}
 
-fs.createReadStream('src/server/data/ElectricityProductionPlant.csv')
+let cantonPower: { [key: string]: number } = {};
+let cantonIDMap: { [key: string]: string } = {};
+
+// Read the canton ID mapping
+fs.createReadStream('src/server/data/cantonIDMapping.csv')
     .pipe(csv())
-    .on('data', (row: PowerData) => {
-        if (row.Canton in cantonPower) {
-            cantonPower[row.Canton] += parseFloat(row.TotalPower);
-        } else {
-            cantonPower[row.Canton] = parseFloat(row.TotalPower);
-        }
+    .on('data', (row: CantonID) => {
+        cantonIDMap[row.name] = row.id;
     })
     .on('end', () => {
-        let output = 'Canton,TotalPower\n';
-        for (let canton in cantonPower) {
-            if (cantonPower.hasOwnProperty(canton)) {
-                let totalPower = cantonPower[canton];
-
-                // Check if totalPower is not undefined before calling toFixed(2)
-                if (totalPower !== undefined) {
-                    let formattedPower = totalPower.toFixed(2);
-                    output += `${canton},${formattedPower}\n`;
+        // Process the power data
+        fs.createReadStream('src/server/data/ElectricityProductionPlant.csv')
+            .pipe(csv())
+            .on('data', (row: PowerData) => {
+                if (row.Canton in cantonPower) {
+                    cantonPower[row.Canton] += parseFloat(row.TotalPower);
+                } else {
+                    cantonPower[row.Canton] = parseFloat(row.TotalPower);
                 }
-            }
-        }
-        fs.writeFileSync('src/server/data/cantonsTotalPower.csv', output);
-        console.log('CSV file successfully processed');
+            })
+            .on('end', () => {
+                let output = 'ID,Canton,TotalPower\n';
+                for (let canton in cantonPower) {
+                    if (cantonPower.hasOwnProperty(canton)) {
+                        let totalPower = cantonPower[canton];
+
+                        // Check if totalPower is not undefined before calling toFixed(2)
+                        if (totalPower !== undefined) {
+                            let formattedPower = totalPower.toFixed(2);
+                            let id = cantonIDMap[canton];
+                            output += `${id},${canton},${formattedPower}\n`;
+                        }
+                    }
+                }
+                fs.writeFileSync('src/server/data/cantonsTotalPower.csv', output);
+                console.log('CSV file successfully processed');
+            });
     });
