6cba7599b697cd98f4a050658786404cb8e97710
LivClaraVictoria <livweiss95@gmail.com>
2023-12-14 18:23:32 +0100

    changed usGeo to swissGeo (also just checking that pushing works as expected)

--- src/client/state/hooks.ts
+++ src/client/state/hooks.ts
@@ -1,25 +1,25 @@
 import { feature } from "topojson";
-import usGeo from "../data/swiss-maps.json";
+import swissGeo from "../data/swiss-maps.json";
 
 
 export function useSwissAtlas() {
   const state = feature(
-      usGeo as any,
-      usGeo.objects.country as TopoJSON.GeometryCollection,
+      swissGeo as any,
+      swissGeo.objects.country as TopoJSON.GeometryCollection,
     );
     //console.log("Processed Features:", processedFeatures);
 
 
  const cantons = feature(
-      usGeo as any,
-      usGeo.objects.cantons as TopoJSON.GeometryCollection,
+      swissGeo as any,
+      swissGeo.objects.cantons as TopoJSON.GeometryCollection,
     );
     //console.log("Processed Canton Borders:", processedCantonBorders);
 
 
   const municipalities = feature(
-      usGeo as any,
-      usGeo.objects.municipalities as TopoJSON.GeometryCollection,
+      swissGeo as any,
+      swissGeo.objects.municipalities as TopoJSON.GeometryCollection,
     );
     //console.log("Processed Municipality Borders:", processedMunicipalityBorders);
 
