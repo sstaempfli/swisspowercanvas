2cfba24b1eb09cab52103137993c126c65ed5673
LivClaraVictoria <livweiss95@gmail.com>
2023-12-14 21:50:47 +0100

    formatted numbers, added N/A when no energy of that type

--- src/client/colorlegend.tsx
+++ src/client/colorlegend.tsx
@@ -1,20 +1,35 @@
 import { scaleSequential, interpolateViridis } from "d3";
+import {useEffect } from "react";
 
 interface ColorLegendProps {
   min: number;
   max: number;
 }
 
+const formatter = new Intl.NumberFormat('en-US', {
+  maximumFractionDigits: 0, 
+});
+
+
 const ColorLegend: React.FC<ColorLegendProps> = ({ min, max }) => {
   const colorScale = scaleSequential(interpolateViridis).domain([0, 1]);
+  const minText =isFinite(max) ? formatter.format(min) + " kW" : "N/A"; //displays N/A on scale if no energy of that type produced in CH
+  const maxText =isFinite(max) ? (formatter.format(max) + " kW") : "N/A";
+
+
+  //see when max changes
+  useEffect(() => {
+    console.log(`Max value changed: ${max}`);
+  }, [max]);
+
 
   return (
     <svg width="300" height="80">
       <text x="0" y="40">
-        {min}
+        {minText}
       </text>
       <text x="300" y="40" textAnchor="end">
-        {Math.round(max)}
+        {maxText}
       </text>
       {[...Array(300).keys()].map((value) => (
         <rect
