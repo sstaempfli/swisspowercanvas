555d35351c49cd48633710b6ebb9eab87bc8cc54
Noah <noah.fecker@gmail.com>
2023-12-21 22:38:35 +0100

    clarifications about the data in the info section

--- src/client/Info.tsx
+++ src/client/Info.tsx
@@ -3,9 +3,14 @@ function Info(_props: any) {
     <article>
       <div>
         <p>
-          We use a logarithmic scale to display the different power output
-          levels, as a linear scale would be too sensitive to extremely high
-          power output.
+          The data always displays the highest possible power output of a power
+          plant, which might not be reached at all times. Power plants that are
+          not are not in operation anymore are not displayed in the bar chart.
+        </p>
+        <p>
+          We use a logarithmic scale for the color to display the different
+          power output levels, as a linear scale would be too sensitive to
+          extremely high power output.
         </p>
         <p>
           The data is sourced from the Swiss Federal Office of Topography
