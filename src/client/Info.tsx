829d5b8dc8815402503233b93416f2861e4a4b45
Noah <noah.fecker@gmail.com>
2023-12-21 22:20:15 +0100

    fix html of info button

--- src/client/Info.tsx
+++ src/client/Info.tsx
@@ -1,14 +1,25 @@
-function Info(_props: any){
-    return (
-        <article>
-            <body>
-                <p>We use a logarithmic scale to display the different power output levels, as a linear scale would be too sensitive to extremely high power output.</p>
-                <p>The data is sourced from the Swiss Federal Office of Topography (swisstopo), ensuring the highest standards of data accuracy and reliability.</p>
-                <p>Data Source:</p>
-                <a href="https://opendata.swiss/de/dataset/elektrizitatsproduktionsanlagen">https://opendata.swiss/de/dataset/elektrizitatsproduktionsanlagen</a><span> (last updated on 5.12.23)</span>
-            </body>
-        </article>
-    )
+function Info(_props: any) {
+  return (
+    <article>
+      <div>
+        <p>
+          We use a logarithmic scale to display the different power output
+          levels, as a linear scale would be too sensitive to extremely high
+          power output.
+        </p>
+        <p>
+          The data is sourced from the Swiss Federal Office of Topography
+          (swisstopo), ensuring the highest standards of data accuracy and
+          reliability.
+        </p>
+        <p>Data Source:</p>
+        <a href="https://opendata.swiss/de/dataset/elektrizitatsproduktionsanlagen">
+          https://opendata.swiss/de/dataset/elektrizitatsproduktionsanlagen
+        </a>
+        <span> (last updated on 5.12.23)</span>
+      </div>
+    </article>
+  );
 }
 
 export default Info;
