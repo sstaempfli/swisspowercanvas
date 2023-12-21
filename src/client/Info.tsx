34855f7976b7b6d090972270cdf91297a00f9d9d
LivClaraVictoria <livweiss95@gmail.com>
2023-12-21 17:06:27 +0100

    added some bs to info box to make it less empty

--- src/client/Info.tsx
+++ src/client/Info.tsx
@@ -3,6 +3,7 @@ function Info(_props: any){
         <article>
             <body>
                 <p>We use a logarithmic scale to display the different power output levels, as a linear scale would be too sensitive to extremely high power output.</p>
+                <p>The data is sourced from the Swiss Federal Office of Topography (swisstopo), ensuring the highest standards of data accuracy and reliability.</p>
                 <p>Data Source:</p>
                 <a href="https://opendata.swiss/de/dataset/elektrizitatsproduktionsanlagen">https://opendata.swiss/de/dataset/elektrizitatsproduktionsanlagen</a><span> (last updated on 5.12.23)</span>
             </body>
